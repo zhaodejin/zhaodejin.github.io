19e5ff873f666213e9d4b574f72b02c045d7a337
zhaoddejin <dejin.zhao@7x-networks.com>
2022-03-08 20:18:40 +0800

    +13900

--- save.js
+++ save.js
@@ -24,7 +24,7 @@ function finalFormat(seconds) {
 
 function update() {
     var startSeconds = 1626660000;
-    var save = 32000 + 4828.14 + 2000 + 1500 + 2000 + 23500;
+    var save = 32000 + 4828.14 + 2000 + 1500 + 2000 + 13900;
     var id = setInterval(flush, 1000)
 
     function flush() {
